19b6f5d1beabb32efd1e774dad986a7fbba4e258
ewdgeniy <ewdgeniy@gmail.com>
2020-05-20 14:56:08 +0300

    initial

---
+++ src/components/static.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Static extends React.Component {
+  render() {
+    return (
+      
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+//========================================
+
+ReactDOM.render(
+  <Static />,
+  document.getElementById('root')
+);
